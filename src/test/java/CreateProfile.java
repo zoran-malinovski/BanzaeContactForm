c94ee5470e2a2925b4a7a330498347c549bc1fa3
Zoran <z.malinovski79@gmail.com>
2023-05-17 17:02:56 +0200

    added first Test for Contact form

--- src/test/java/CreateProfile.java
+++ src/test/java/CreateProfile.java
@@ -8,7 +8,7 @@ public class CreateProfile {
 
 
     @Test
-    public static void main(String[] args) {
+    public void CreateProfiles() throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver", "C:\\Chrome Drivers\\chromedriver.exe");
         WebDriver driver;
@@ -18,5 +18,36 @@ public class CreateProfile {
 
         WebElement element = driver.findElement(By.xpath("//a[@class='MuiButtonBase-root MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-sizeMedium MuiButton-textSizeMedium MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-sizeMedium MuiButton-textSizeMedium css-1enydpc']"));
         element.click();
+        Thread.sleep(3000);
+
+        WebElement element1 = driver.findElement(By.name("firstName"));
+        element1.sendKeys("Zoran");
+        Thread.sleep(1500);
+
+        WebElement element2 = driver.findElement(By.name("lastName"));
+        element2.sendKeys("Malinovski");
+        Thread.sleep(1500);
+
+        WebElement element3 = driver.findElement(By.name("email"));
+        element3.sendKeys("zoran@mail.com");
+        Thread.sleep(1500);
+
+        WebElement element4 = driver.findElement(By.name("phoneNumber"));
+        element4.sendKeys("+38970123123");
+        Thread.sleep(1500);
+
+        WebElement element5 = driver.findElement(By.name("additionalInfo"));
+        element5.sendKeys("Dobar den Banzae, najbolji ste");
+        Thread.sleep(1500);
+
+
+
+        WebElement element6 = driver.findElement(By.xpath("//button[normalize-space()='Submit']"));
+        element6.submit();
+        driver.navigate().refresh();
+
+
+
+
     }
 }
